e3e5adc673d0ee9bd45723b71b9aba5552705260
xander <izabalexis@gmail.com>
2026-09-18 18:20:19 +0200

    first commit calcul

---
+++ calcul.js
@@ -0,0 +1,19 @@
+
+
+let r=5;
+const pi=3.14;
+let area=r*r*pi;
+console.log(`Area of the Circle is: ${area}`)
+
+let a=10;
+let b=15;
+console.log(a+b,a-b,a*b,a/b,a%b);
+if (a<b){
+    console.log(` ${a} is less than ${b}`)
+}       
+        else if (a>b){console.log(` ${a} is greater than ${b}`)
+        }
+        else {console.log(` ${a} is equal to ${b}`)
+    }                       
+
+
