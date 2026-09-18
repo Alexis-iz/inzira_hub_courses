c741c8cb776d001d1009cac09b4fad339f212a92
xander <izabalexis@gmail.com>
2026-09-18 17:06:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+import boxen from "boxen"; 
+import chalk from "chalk";
+
+
+console.log(chalk.blue(`My name is ${chalk.bold("Alexis")}.`));
+console.log(chalk.green.italic(`I am learning ${chalk.bold.yellow("Node.js")}.`));
+console.log(chalk.green.italic("I am excited to appply these skills to my on going projects"));
+console.log(chalk.green.italic("Day 1 is complete!"));
+console.log(chalk.green.italic("Thank you for your cooperation!."));
+
