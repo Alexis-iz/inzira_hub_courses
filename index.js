d4500e5b70ad67b0669ebecdd56cda5c4f37a2ad
xander <izabalexis@gmail.com>
2026-09-19 12:02:24 +0200

    calcul added for test

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ console.log(chalk.green.italic("I am excited to appply these skills to my on goi
 console.log(chalk.green.italic("Day 1 is complete!"));
 console.log(chalk.green.italic("Thank you for your cooperation!."));
 
+let r=5;
+const pi=3.14;
+let area=r*r*pi;
+console.log(`Area of the Circle is: ${area}`)
+
+let a=10;
+let b=15;
+console.log(a+b,a-b,a*b,a/b,a%b);
+if (a<b){
+    console.log(` ${a} is less than ${b}`)
+}       
+        else if (a>b){console.log(` ${a} is greater than ${b}`)
+        }
+        else {console.log(` ${a} is equal to ${b}`)
+    }                       
+
